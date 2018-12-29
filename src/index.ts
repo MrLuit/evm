bc0c9ae4af2d141f010ac4a831c275dd3285b596
Luit Hollander <luit@hollander.email>
2018-12-29 23:31:11 +0100

    Update default export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 import EVM from './classes/evm.class';
 
-export default (byteCode: any, debug = false) => {
-    const evm = new EVM(byteCode);
-    return evm.toString(debug);
-};
+export default EVM;
